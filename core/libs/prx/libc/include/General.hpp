72af167a9ff28632c9e1951c877ea6609a9dcafc
boykopovar <boykopovar@gmail.com>
2026-09-06 15:26:09 +0300

    fix(libc): add extern c for not impl helper header

--- core/libs/prx/libc/include/General.hpp
+++ core/libs/prx/libc/include/General.hpp
@@ -1,6 +1,6 @@
 #ifndef CORE_LIBS_PRX_LIBC_INCLUDE_GENERAL_HPP
 #define CORE_LIBS_PRX_LIBC_INCLUDE_GENERAL_HPP
 
-void NotImplemented_nid_no_patch(const char* funcName);
+extern "C" void NotImplemented_nid_no_patch(const char* funcName);
 
 #endif
